43786d0b812dd68be8e2b9d28e5672a4f54a7d72
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-01-31 15:25:29 +0300

    Added src/CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,7 @@
 public class CreditPaymentService {
     public double calculate(int creditAmount, double percent, int months) {
-        double monthPercent = percent/months/100;
+        int year = 12;
+        double monthPercent = percent/year/100;
         double monthlyPayment = creditAmount * monthPercent * Math.pow((1+monthPercent),months)/(Math.pow((1+monthPercent),months)-1);
         return (long) monthlyPayment;
     }
